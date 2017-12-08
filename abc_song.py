945c17491afb3c03aa7b7831ed625c0bb03af012
Ricky McCallum <rickymccallum87@gmail.com>
2017-12-08 18:12:33 -0500

    Reverse alphabet with slice and -1 step

---
+++ abc_song.py
@@ -0,0 +1,9 @@
+# sing abc's forward and backward
+# sing function for cadence
+# slice with -1 step to reverse
+
+abc = list('abcdefghijklmnopqrstuvwxyz')
+cba = abc[::-1]
+
+print(abc)
+print(cba)
