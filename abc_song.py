6cef2f6247098c1bdc38e992d29a826bde6c57ac
Ricky McCallum <rickymccallum87@gmail.com>
2017-12-08 18:24:01 -0500

    Recite ABCs forwards and backwards

--- abc_song.py
+++ abc_song.py
@@ -1,9 +1,29 @@
-# sing abc's forward and backward
-# sing function for cadence
-# slice with -1 step to reverse
-
 abc = list('abcdefghijklmnopqrstuvwxyz')
+
+# slice with -1 step to reverse
 cba = abc[::-1]
 
-print(abc)
-print(cba)
+# sing function for cadence
+def sing(alpha):
+	for i in alpha:
+		# current spot
+		th = alpha.index(i)
+
+		print(i, end='')
+
+		if th == 6 or th == 18 or th == 21 or th == 23:
+			print(', ', end='')
+		elif th > 10 and th < 15:
+			print('-', end='')
+		elif th == 15:
+			print('. ', end='')
+		elif th == 24:
+			print(' and ', end='')
+		else:
+			print(' ', end='')
+			
+	print('\n')
+
+# sing abc's forward and backward
+sing(abc)
+sing(cba)
