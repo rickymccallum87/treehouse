9b8e0b2d748791d7dcca77fa4419d99b5e57055f
Ricky McCallum <rickymccallum87@gmail.com>
2017-12-08 15:10:38 -0500

    Working hangman game

--- hangman.py
+++ hangman.py
@@ -1,12 +1,40 @@
-# track errors
-# allow guesses
-# show progress
-# win/lose
-
 import random
 
 # randomly select from list of words
 words = ['cat', 'dog', 'hen', 'pie', 'bull']
 answer = words[random.randint(0, len(words) - 1)]
+# print(answer) # debug
 
-print(answer)
+# track errors and guesses
+errors = 0
+guesses = []
+
+while True:
+	won = True
+
+	# guess
+	guess = input('guess? ')
+
+	if guess in answer:
+		# record correct guess
+		guesses.append(guess)
+
+		# show progress
+		for char in answer:
+			if char in guesses:
+				print(char, end='')
+			else:
+				won = False
+				print('_', end='')
+		print('\n', end='')
+		# win condition
+		if won:
+			print('well done!')
+			break
+	else:
+		errors += 1
+		print('nope. ' + str(errors) + ' wrong guesses')
+		if errors > 5:
+			print('you lose')
+			break
+
