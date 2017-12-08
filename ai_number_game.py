90005845dfc0f627695450e48a29a386fc5d93c3
Ricky McCallum <rickymccallum87@gmail.com>
2017-12-08 14:09:04 -0500

    Base guesses on possible range

--- ai_number_game.py
+++ ai_number_game.py
@@ -1,17 +1,23 @@
 # ask for number
 answer = int(input('secret number (1-10)? '))
 
-# default guess
-guess = 5
+# possible range
+between = [1,10]
 while True:
+	# think out loud
+	print('between ' + str(between[0]) + ' and ' + str(between[1]) + '..')
+
+	# guess
+	guess = (between[0] + between[1]) // 2
+
 	# get feedback
 	feedback = input('is it ' + str(guess) + ' (h/l/Y)? ')
 	
 	# adjust guess
 	if feedback == 'h':
-		guess = guess // 2
+		between[1] = guess - 1
 	elif feedback == 'l':
-		guess = guess + (10 - guess) // 2
+		between[0] = guess + 1
 	# celebrate
 	else:
 		print('booyah')
