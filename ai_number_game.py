9cabc2978cb650c88f033e773f9862a1bbc9bce3
Ricky McCallum <rickymccallum87@gmail.com>
2017-12-08 14:12:51 -0500

    Check for cheating player

--- ai_number_game.py
+++ ai_number_game.py
@@ -3,9 +3,15 @@ answer = int(input('secret number (1-10)? '))
 
 # possible range
 between = [1,10]
+
 while True:
 	# think out loud
 	print('between ' + str(between[0]) + ' and ' + str(between[1]) + '..')
+	
+	# play fair check
+	if between[0] > between[1]:
+		print('cheater!')
+		break
 
 	# guess
 	guess = (between[0] + between[1]) // 2
