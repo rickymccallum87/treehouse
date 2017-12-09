1f0539c1b73965c81538f93f7852c31dafa74d11
Ricky McCallum <rickymccallum87@gmail.com>
2017-12-09 09:24:22 -0500

    Begin dungeon game with empty game loop

---
+++ dungeon_game.py
@@ -0,0 +1,40 @@
+import random
+
+# draw player
+# take movement input
+# move player, within bounds
+# check for win/loss
+# clear screen and redraw grid
+
+# draw the grid
+CELLS = [(0,0), (1,0), (2,0), (3,0), (4,0), 
+	 (0,0), (1,1), (2,1), (3,1), (4,1), 
+	 (0,0), (1,2), (2,2), (3,2), (4,2), 
+	 (0,0), (1,3), (2,3), (3,3), (4,3), 
+	 (0,0), (1,4), (2,4), (3,4), (4,4)]
+
+def get_locations():
+	# pick random location for player
+	# pick random location for exit door
+	# pick random location for monster
+	player = None
+	monster = None
+	door = None
+	return monster, door, player
+
+while True:
+	print('Welcome!')
+	print('You\'re currently in room ()') # format player pos
+	print('You can move {}') # format available moves
+	print('Enter QUIT to quit')
+
+	move = input('> ').lower()
+	
+	if move == 'quit':
+		break
+
+	# good? change pos
+	# bad? don't change
+	# on door? win!
+	# on monster? lose!
+	# else loop
