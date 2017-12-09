d8cec669c3dad8ac4d667b1948fd47fcc0ed5361
Ricky McCallum <rickymccallum87@gmail.com>
2017-12-09 05:10:49 -0500

    Clear screen on update

--- shopping.py
+++ shopping.py
@@ -1,6 +1,14 @@
+# add to desired index with .insert()
+import os
+
 items = []
 
+# clear screen on update
+def clear():
+	os.system('cls' if os.name == 'nt' else 'clear')
+
 def instruct():
+	clear()
 	print('Create your shopping list.')
 	print(
 '''These commands are supported:
@@ -9,9 +17,15 @@ show > print list and continue
 help > see these instructions''')
 
 def display():
+	clear()
 	for i in items:
 		print(i)
 	
+def add(item):
+	items.append(item)
+	display()
+	print('{} added. {} items total'.format(item, len(items)))
+
 while True:
 	item = input('New item? ')
 	if item == 'done':
@@ -23,6 +37,6 @@ while True:
 	elif item == 'help':
 		instruct()
 	else:
-		items.append(item)
+		add(item)
 
 display()
