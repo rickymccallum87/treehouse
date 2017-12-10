3f570fbe0da52eefe1ce470a7af203bca5ff4e5c
Ricky McCallum <rickymccallum87@gmail.com>
2017-12-10 16:53:39 -0500

    Add help and list features. Disperse into functions.

--- todo.py
+++ todo.py
@@ -1,10 +1,26 @@
 todo_list = []
 
-while True:
-    item = input('> ')
-    if item == 'd':
-        break
-    else:
-        todo_list.append(item)
+def show_help():
+    print('(H)elp, (L)ist, (Q)uit')
 
-print(todo_list)
+def show_list():
+    print(todo_list)
+
+def add_item(item):
+    todo_list.append(item)
+
+def main():
+    show_help()
+    while True:
+        item = input('> ')
+        if item == 'h':
+            show_help()
+        elif item == 'l':
+            show_list()
+        elif item == 'q':
+            break
+        else:
+            add_item(item)
+
+main()
+show_list()
