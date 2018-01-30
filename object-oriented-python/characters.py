6f8f6ddad8e9ea1050bf37af9df133a98d700c27
Ricky McCallum <rickymccallum87@gmail.com>
2018-01-30 17:32:30 +0000

    Add Enemy class

--- object-oriented-python/characters.py
+++ object-oriented-python/characters.py
@@ -1,10 +1,25 @@
 class Mage:
   mana = 10
 
-  def fireball(self):
+  def fireball(self, enemy):
     if self.mana > 0:
       self.mana -= 1
-      print('ka-boom!')
+      if enemy.inCombat:
+        enemy.health -= 3
+        print('ka-boom!')
+        if enemy.health < 1:
+          enemy.inCombat = False
+          print('x.x')
+      else:
+        print('whiff!')
     else:
       print('not enough mana')
     return self.mana
+
+class Enemy:
+  health = 10
+  inCombat = True
+
+  def run(self):
+    self.inCombat = False
+    print('eeek!')
